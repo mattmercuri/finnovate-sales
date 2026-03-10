2afd13b8eaee3f1cc001b6c09a756a2cccdca671
Matthew Mercuri <matthew@finnovate.io>
2026-03-10 15:42:57 -0400

    added auth subrouter

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import auth from './auth.js'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.route('/auth', auth)
 
 serve({
   fetch: app.fetch,
