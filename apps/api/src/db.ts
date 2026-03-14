c4efaa85b83ac9894532aedd637348be4763f506
Matthew Mercuri <matthew@finnovate.io>
2026-03-14 16:59:38 -0400

    fixed issue where export for Prisma was not working

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -1,3 +1,3 @@
-import { prisma } from '@finnovate-sales/db'
-export type DB = typeof prisma
+import { prisma, type DB } from '@finnovate-sales/db'
+export type { DB }
 export default prisma
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient, type PrismaClient as PrismaClientType } from "./generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma: PrismaClientType = new PrismaClient({ adapter });
 
 export { prisma };
+export type DB = PrismaClientType;
