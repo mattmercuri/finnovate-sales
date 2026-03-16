7f7603f2972f61b192cf448be0af22781ab0af47
Matthew Mercuri <matthew@finnovate.io>
2026-03-16 12:08:41 -0400

    updated orval setup

--- apps/web/orval.config.js
+++ apps/web/orval.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'orval'
 export default defineConfig({
   api: {
     output: {
-      mode: 'split-tags',
-      target: './src/api.ts',
+      mode: 'tags-split',
+      target: './src/api',
       schemas: './src/schemas',
       client: 'react-query',
     },
